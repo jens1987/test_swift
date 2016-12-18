94b4fc7b7b8e014e9c50eb25110742ac0ae80e84
Jens Kühnapfel <jenskuhnapfel@jenss-macbook-air.local>
2016-12-18 22:18:15 +0100

    Initial Commit

---
+++ Swift_App_test_0.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_App_test_0.1
+//
+//  Created by Jens Kühnapfel on 18.12.16.
+//  Copyright © 2016 Jens Kühnapfel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
