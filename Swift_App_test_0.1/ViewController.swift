9cb400a7c3dca2c63f704d3e516c2ccba93dcee4
Jens Kühnapfel <jens.kuehnapfel@icloud.com>
2017-01-01 13:29:56 +0100

    no message

--- Swift_App_test_0.1/ViewController.swift
+++ Swift_App_test_0.1/ViewController.swift
@@ -3,19 +3,34 @@
 //  Swift_App_test_0.1
 //
 //  Created by Jens Kühnapfel on 18.12.16.
-//  Copyright © 2016 Jens Kühnapfel. All rights reserved.
+//  Copyright © 2016 Jens Kühnapfel. All rights reserved...
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+            tapCount = tapCount + 1
+        if tapCount >= 20 {
+                theLabel.text = ("You tapped the button 20 times")
+        }
+        }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.theLabel.text = "Hello there!"
     }
-
-    override func didReceiveMemoryWarning() {
+    
+  override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
